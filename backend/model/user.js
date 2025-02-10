d411c1e6800e8e223b74fa04b444f0023c5d91b1
Manya <mayavi1319@gmail.com>
2025-02-10 10:49:14 +0530

    Milestone_4

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
-
 const userSchema = new mongoose.Schema({
   name:{
     type: String,
@@ -64,33 +62,23 @@ const userSchema = new mongoose.Schema({
  resetPasswordToken: String,
  resetPasswordTime: Date,
 });
-
-
-
-
 //  Hash password
-userSchema.pre("save", async function (next){
-  if(!this.isModified("password")){
-    next();
-  }
-
-
-  this.password = await bcrypt.hash(this.password, 10);
-});
-
-
+// userSchema.pre("save", async function (next){
+//   if(!this.isModified("password")){
+//     next();
+//   }
+//   this.password = await bcrypt.hash(this.password, 10);
+// });
 // jwt token
-userSchema.methods.getJwtToken = function () {
-  return jwt.sign({ id: this._id}, process.env.JWT_SECRET_KEY,{
-    expiresIn: process.env.JWT_EXPIRES,
-  });
-};
-
-
-// compare password
-userSchema.methods.comparePassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
+// userSchema.methods.getJwtToken = function () {
+//   return jwt.sign({ id: this._id}, process.env.JWT_SECRET_KEY,{
+//     expiresIn: process.env.JWT_EXPIRES,
+//   });
+// };
+// // compare password
+// userSchema.methods.comparePassword = async function (enteredPassword) {
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
 
 
 module.exports = mongoose.model("User", userSchema);
